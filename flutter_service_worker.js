4290c83381584fad471edd894a0932cbb5c99725
Ravi Ranjan <ravi.ranjan@borqsindia.com>
2025-08-05 11:44:49 +0530

    student_module

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -32,14 +32,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "009d0a8aa3a98c3a6ae9f309b173b095
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "6b6b1cdb9ed01043fb88037f1f183f2d",
+"flutter_bootstrap.js": "acfd9b825bac656ad0c505779e41c2a5",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "023543ca9a48db34148480892ddc0f72",
 "/": "023543ca9a48db34148480892ddc0f72",
-"main.dart.js": "8e107fa25c29bb48b9bdf445f329e85a",
+"main.dart.js": "4fb46f179115fe103c4f621d3b6fa3d4",
 "manifest.json": "a86d7cf2ba3dca9d48fa4cb7802a4ad1",
 "version.json": "230b47caf1b9b3ba06505099eeb4846f"};
 // The application shell files that are downloaded before a service worker can
