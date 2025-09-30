ecf39514b2372a033ce64ca7817ea58793ecc916
Ravi Ranjan <ravi.ranjan.bsy@gmail.com>
2025-09-30 14:35:20 +0530

    0930_add_teacher_working

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"ddf47dd3ff96dbde6d9c614db0d7f019d7c7a2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "328388871"
+    serviceWorkerVersion: "1231979170"
   }
 });
